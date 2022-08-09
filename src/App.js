6e9c1c065e6690a8e41417c3e569f74e925af844
Yogya Singhal <yogya.singhal@globallogic.com>
2022-08-09 10:34:55 +0530

    make simple counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import SimpleState from './StateFunctional/SimpleState';
 function App() {
+  console.log("app.js");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-black text-light">
+     <h1>hello function</h1>
+     <SimpleState/>
     </div>
   );
 }
---
+++ src/StateFunctional/SimpleState.js
@@ -0,0 +1,36 @@
+import {useState} from "react"; 
+const SimpleState = (props)=>{
+    console.log("hello");
+    const [count,setCount] = useState(0);
+    const [inp,setInp] = useState('');
+    const [ans,setAns] = useState('');
+    const handleChange=(e)=>{
+        e.preventDefault();
+        setInp(e.target.value);
+    }
+    const showInput=(e)=>{
+        // console.log("btn clicked");
+        e.preventDefault();
+        setAns(inp);
+    }
+    return(
+        <div>
+            <h1>{inp}</h1>
+            <h1>{ans}</h1>
+            <div className="d-flex justify-content-center m-4 p-4">
+            {console.log(useState(100))}
+            <input
+            type="text" className="form-control w-25" placeholder="Enter Text" 
+            onChange={(e)=>handleChange(e)}></input>
+            <button onClick={(e)=>showInput(e)} className="mx-4 btn btn-success">Submit</button>
+        </div>
+        <div className="d-flex justify-content-evenly m-4 p-4">
+            {/* <h1> Simple State</h1> */}
+            <button className="btn btn-danger btn-lg" onClick={()=>setCount(count-1)}>Subtract</button>
+            <h1>{count}</h1>
+            <button className="btn btn-primary btn-lg" onClick={()=>setCount(count+1)}>Add</button>
+        </div>
+        </div>
+    )
+}
+export default SimpleState;
